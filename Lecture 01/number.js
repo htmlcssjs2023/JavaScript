37cee8751761f2beeb9aab364f302440136843f3
mdabdulhalim <htmlcssjs2023>
2023-07-23 18:24:53 +0600

    Numeric Separator

--- Lecture 01/number.js
+++ Lecture 01/number.js
@@ -1,1 +1,24 @@
 console.log('Number');
+
+// Integer Number 
+let number = 299;
+console.log(number);
+
+// Octal Base number
+// let num = 071;
+// console.log(num);
+
+// let num = 080;
+// console.log(num);
+
+// let amount = 5e-7; 
+// console.log(amount);
+
+// let amount = 0.0000005;
+// console.log(amount);
+
+
+// let amount = 0.2 + 0.1;
+// console.log(amount);
+
+// let pageView = 9007199254740991n;
